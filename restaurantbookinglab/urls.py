3b267e92bd1da6669d0b23a4acc58ac8f5ebd971
Sergio Vargas <sergiovl219@gmail.com>
2023-11-05 18:46:34 -0600

    Api token auth implementation.

--- restaurantbookinglab/urls.py
+++ restaurantbookinglab/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken import views as rest_framework_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-token-auth', rest_framework_views.obtain_auth_token)
 ]
