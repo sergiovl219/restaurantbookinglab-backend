d8a455decc09f9175d9cb8ba6a5fff0736205631
Sergio Vargas <sergiovl219@gmail.com>
2023-11-07 16:31:38 -0600

    OperationalError caught in task.

--- discount_tickets/tasks.py
+++ discount_tickets/tasks.py
@@ -1,18 +1,22 @@
 from celery import shared_task
+from django.db import OperationalError
 from django.db import transaction
 
+from discount_tickets.exceptions.custom_exceptions import TicketNotAvailableException
 from discount_tickets.helpers.ticket_helper import get_ticket_by_id
 from discount_tickets.serializers.purchase_serializers import PurchaseSerializer
 
 
 @shared_task
 def process_ticket_purchase(creation_data: dict):
-    with transaction.atomic():
-        serializer = PurchaseSerializer(data=creation_data)
-        serializer.is_valid(raise_exception=True)
-        ticket = get_ticket_by_id(creation_data["ticket"])
-        try:
+    try:
+        with transaction.atomic():
+            serializer = PurchaseSerializer(data=creation_data)
+            serializer.is_valid(raise_exception=True)
+            ticket = get_ticket_by_id(creation_data["ticket"])
             serializer.save(ticket=ticket, guest=None)
             return serializer.data
-        except Exception as e:
-            raise e
+    except OperationalError:
+        raise TicketNotAvailableException(f"Ticket {creation_data['ticket']} is not available.")
+    except Exception as e:
+        raise e
